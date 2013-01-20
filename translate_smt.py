05274d8107b6848e7d9e478fc6d4bfb6e9b5a9ef
Billy Melicher <wrm2ja@virginia.edu>
2013-01-20 16:40:10 -0500

    changed way non-width numbers are provided

--- translate_smt.py
+++ translate_smt.py
@@ -12,39 +12,6 @@ vars = {}
 int_re = re.compile("^-?([0-9])+(:([0-9]+))?$")
 find_ranges = {}
 
-def smt_proc_write( val ):
-    log_file.write(val)
-    smt_proc.stdin.write(val)
-
-def introduce_var(name,rng):
-    global vars
-    vars[name] = rng
-    smt_proc_write("(declare-fun "+name+" () (_ BitVec "+str(rng)+"))\n")
-
-def arg_to_smt(arg):
-    match = int_re.match(arg)
-    if match == None:
-        return (arg,vars[arg])
-    size = int(match.group(3))
-    return ( "(nat2bv["+str(size)+"] "+match.group(1)+")", size )
-
-def const_bv_literal( num, size ):
-    raw = bin(num)[2:]
-    raw = "0"*(size-len(raw))
-    return "#b"+raw
-
-def const_arg_to_smt( arg ):
-    match = int_re.match(arg)
-    size = int(match.group(3))
-    return ( match.group(1), size )
-
-def assert_smt(statement):
-    return "(assert "+statement+")\n"
-
-def assign_smt(statement,out):
-    return "(= "+out+" "+statement+")"
-
-# still need bitwise operations
 same_len_ops = {
    "add" : "bvadd",    
    "sub" : "bvadd", 
@@ -64,7 +31,49 @@ one_bit_ops = {
     "gts" : (True, False, False),   
     "gtu" : (False, False, False) }
 
-ops_set = same_len_ops.viewkeys() | one_bit_ops.viewkeys()
+############# utility methods
+def smt_proc_write( val ):
+    log_file.write(val)
+    smt_proc.stdin.write(val)
+
+def introduce_var(name,rng):
+    global vars
+    vars[name] = rng
+    smt_proc_write("(declare-fun "+name+" () (_ BitVec "+str(rng)+"))\n")
+
+def const_to_bv( match ):
+    mag = int(match.group(1))
+    size = match.group(3)
+    if size == None:
+        size = len(bin(mag))-2
+    return ( const_bv_literal(mag,int(size)), int(size) )
+
+def arg_to_smt(arg):
+    match = int_re.match(arg)
+    if match == None:
+        return (arg,vars[arg])
+    return const_to_bv( match )
+
+def const_bv_literal( num, size ):
+    raw = bin(num)[2:]
+    raw = ( "0"*(size-len(raw) ) ) + raw
+    return "#b"+raw
+
+def const_arg_to_smt( arg ):
+    match = int_re.match(arg)
+    mag = int( match.group(1) )
+    size = match.group(3)
+    if size == None:
+        size = len(bin(mag))-2
+    return ( mag, int(size) )
+
+def assert_smt(statement):
+    return "(assert "+statement+")\n"
+
+def assign_smt(statement,out):
+    return "(= "+out+" "+statement+")"
+
+########### end utility methods
 
 def comparison(a, b, is_signed, is_lt, is_eq):
     ret = "(bvult "
@@ -91,10 +100,7 @@ def map_ops(op, args):
         retval += same_len_ops[op]
     else:
         compvals = one_bit_ops[op]
-        retval = comparison( args[0], args[1],
-                             compvals[0],
-                             compvals[1],
-                             compvals[2] )
+        retval = comparison( args[0], args[1], compvals[0], compvals[1], compvals[2] )
         arg_size = 1
         return ( retval, arg_size )
     for i in xrange(len(args)):
@@ -102,6 +108,8 @@ def map_ops(op, args):
         vals_bv_form = vals[0]
         if i == 0 and op == "chose":
             retval += " (= "+vals_bv_form+" #b1)"
+        elif i == 1 and op == "sub":
+            retval += " (bvneg "+vals_bv_form+")"
         else:
             retval += " "+vals_bv_form
         if op in same_len_ops:
@@ -127,6 +135,13 @@ def sp_shiftr(args):
 # bits_special = {
 #             "concatls" : sp_concatls, 
 #             "decode" : sp_decode, 
+
+def sp_decode( args ):
+    # number and enable
+    arg1 = arg_to_smt( args[0] )
+    enable = arg_to_smt( args[1] )
+    zeros = const_bv_literal( 0, 2**arg1[1] )
+    return ( "(ite (= "+enable[0]+" #b1) "+
 
 def sp_concat(args):
     ret = ""
@@ -156,18 +171,15 @@ def sp_sextend( args ):
     # TODO: handle signed arguments
     to_arg = const_arg_to_smt( args[1] )
     base_arg = arg_to_smt( args[0] )
-    ret = "(concat "+base_arg[0]+" #b"+("0"*(to_arg[0]-base_arg[1]))+")"
+    ret = "(concat #b"+("0"*(to_arg[0]-base_arg[1]))+" "+base_arg[0]+")"
     return ( ret, to_arg[0] )
-
-def sp_trunc( args ):
-    return sp_sextend( [ args[0], "0:1" ] )
 
 special = { "shiftl" : sp_shiftl,
             "shiftr" : sp_shiftr,
             "concat" : sp_concat,
             "select" : sp_select,
             "sextend" : sp_sextend,
-            "trunc" : sp_trunc,
+            "trunc" : lambda arg: sp_sextend( [ arg[0], "0:1" ] ),
             "zextend" : sp_sextend }
 
 def map_il_to_smt(op,args,out):
@@ -187,15 +199,11 @@ def map_il_to_smt(op,args,out):
     assert_clause = assert_smt(assign_clause)
     smt_proc_write( assert_clause ) 
 
-def assert_less(name, upper):
-    return assert_smt( "(ult "+name+" "+const_bv_literal(upper,vars[name])+")" )
-
 def map_dot_to_smt(dot_op, args):
     global vars
     if dot_op == ".input":
         bit_width = int(args[2])
         introduce_var(args[0], bit_width )
-        # smt_proc_write( assert_less(args[0],2**bit_width) )
     if dot_op == ".remove":
         vars.pop( args[1] )
 
@@ -205,10 +213,9 @@ def header():
 (set-logic QF_BV)
 """
 
-
 def make_guess(arg,lessthan):
     smt_proc_write( "(push 1)\n" )
-    smt_proc_write( assert_smt("(bvult "+str(lessthan)+" "+arg+")") )
+    smt_proc_write( assert_smt("(not (bvult "+arg+" "+const_bv_literal(lessthan,vars[arg])+"))") )
     smt_proc_write("(check-sat)\n")
     line = smt_proc.stdout.readline().strip()
     log_file.write( "-> "+line+"\n" )
@@ -221,6 +228,7 @@ def make_guess(arg,lessthan):
         raise Exception("SMT lib exception: "+line)
 
 def search_range(var,end):
+    # TODO: experiment with this search. try binary search
     start = 0
     max_val = end
     while max_val > start and not make_guess(var,2**start):
