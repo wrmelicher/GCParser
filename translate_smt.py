e1d3db1339f39781085f8c8b2ad8529a9a34643d
Billy Melicher <wrm2ja@virginia.edu>
2013-01-20 18:47:54 -0500

    using bit vector functions

--- translate_smt.py
+++ translate_smt.py
@@ -9,7 +9,7 @@ smt_cmd = os.getenv("SMT_LOCATION")
 smt_proc = subprocess.Popen([smt_cmd], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
 vars = {}
-int_re = re.compile("^-?([0-9])+(:([0-9]+))?$")
+int_re = re.compile("^-?([0-9]+)(:([0-9]+))?$")
 find_ranges = {}
 
 same_len_ops = {
@@ -19,17 +19,20 @@ same_len_ops = {
    "chose" : "ite",
    "and" : "bvand",
    "or" : "bvor",
-   "xor" : "bvxor" }
+   "xor" : "bvxor",
+   "not" : "bvnot" }
 
 one_bit_ops = {
-    "ltes" : (True, True, True),
-    "lteu" : (False, True, True),
-    "gtes" : (True, False, True), 
-    "gteu" : (False, False, True), 
-    "lts" : (True, True, False),
-    "ltu" : (False, True, False),
-    "gts" : (True, False, False),   
-    "gtu" : (False, False, False) }
+    "ltes" : (True, True, True, False),
+    "lteu" : (False, True, True, False),
+    "gtes" : (True, False, True, False),
+    "gteu" : (False, False, True, False),
+    "lts" :  (True, True, False, False),
+    "ltu" :  (False, True, False, False),
+    "gts" :  (True, False, False, False),
+    "gtu" :  (False, False, False, False),
+    "equ" :  (False, False, False, True), 
+    "nequ" : (False, False, True, True) }
 
 ############# utility methods
 def smt_proc_write( val ):
@@ -65,7 +68,8 @@ def const_arg_to_smt( arg ):
     size = match.group(3)
     if size == None:
         size = len(bin(mag))-2
-    return ( mag, int(size) )
+    return mag
+#    return ( mag, int(size) )
 
 def assert_smt(statement):
     return "(assert "+statement+")\n"
@@ -75,18 +79,24 @@ def assign_smt(statement,out):
 
 ########### end utility methods
 
-def comparison(a, b, is_signed, is_lt, is_eq):
+def comparison(a, b, params):
+    ( is_signed, is_lt, is_eq_comp, is_equ_fun ) = params
     ret = "(bvult "
     arga = arg_to_smt(a)
     argb = arg_to_smt(b)
+    if is_equ_fun:
+        ret = "(= "+arga[0]+" "+argb[0]+")"
+        if not is_eq_comp:
+            ret = "(not "+ret+")"
+        return ret
     if is_lt:
-        if is_eq:
+        if is_eq_comp:
             ret += argb[0] + " " + arga[0] + ")"
             ret = "(not "+ret+")"
         else:
             ret += arga[0] + " " + argb[0] + ")"
     else:
-        if is_eq:
+        if is_eq_comp:
             ret += argb[0] + " " + arga[0] + ")"
         else:
             ret += arga[0] + " " + argb[0] + ")"
@@ -98,9 +108,9 @@ def map_ops(op, args):
     arg_size = 0
     if op in same_len_ops:
         retval += same_len_ops[op]
-    else:
+    elif op in one_bit_ops:
         compvals = one_bit_ops[op]
-        retval = comparison( args[0], args[1], compvals[0], compvals[1], compvals[2] )
+        retval = comparison( args[0], args[1], compvals )
         arg_size = 1
         return ( retval, arg_size )
     for i in xrange(len(args)):
@@ -121,7 +131,7 @@ def sp_shiftl(args):
     if arg1val == None:
         raise Error("shiftl must shift by constant amount")
     arg2val = arg_to_smt(args[0])
-    state = "(bvshl "+arg2val[0]+" "+const_bv_literal(arg1val[0],arg1val[1])+")"
+    state = "(bvshl "+arg2val[0]+" "+const_bv_literal(arg1val,arg1val[1])+")"
     return (state, arg1val[0] + arg2val[1] )
 
 def sp_shiftr(args):
@@ -129,7 +139,7 @@ def sp_shiftr(args):
     if arg1val == None:
         raise Error("shiftr must shift by constant amount")
     arg2val = arg_to_smt(args[0])
-    state = "(bvshr "+arg2val[0]+" "+const_bv_literal(arg1val[0],arg1val[1])+")"
+    state = "(bvshr "+arg2val[0]+" "+const_bv_literal(arg1val,arg1val[1])+")"
     return (state, arg2val[1] - arg1val[0] )
 
 #TODO: must support decode operation
@@ -163,16 +173,16 @@ def sp_select(args):
     from_arg = const_arg_to_smt(args[1])
     to_arg = const_arg_to_smt( args[2] )
     base_arg = arg_to_smt( args[0] )
-    ret += str(from_arg[0]) + " " + str(to_arg[0]) + ") "
+    ret += str(to_arg-1) + " " + str(from_arg) + ") "
     ret += base_arg[0] + ")"
-    return ( ret, to_arg[0] - from_arg[0] )
+    return ( ret, to_arg - from_arg )
 
 def sp_sextend( args ):
     # TODO: handle signed arguments
     to_arg = const_arg_to_smt( args[1] )
     base_arg = arg_to_smt( args[0] )
-    ret = "(concat #b"+("0"*(to_arg[0]-base_arg[1]))+" "+base_arg[0]+")"
-    return ( ret, to_arg[0] )
+    ret = "(concat #b"+("0"*(to_arg-base_arg[1]))+" "+base_arg[0]+")"
+    return ( ret, to_arg )
 
 special = { "shiftl" : sp_shiftl,
             "shiftr" : sp_shiftr,
@@ -190,6 +200,9 @@ def map_il_to_smt(op,args,out):
         op_clause = map_ops(op,args)
     elif op in special:
         op_clause = special[op](args)
+    else:
+        log_file.write("Error: unknown function "+op+"\n")
+        raise Exception("unknown function "+op)
     introduce_var(out, op_clause[1] )
     if op == "chose":
         find_ranges[out] = op_clause[1]
@@ -205,8 +218,8 @@ def map_dot_to_smt(dot_op, args):
     if dot_op == ".input":
         bit_width = int(args[2])
         introduce_var(args[0], bit_width )
-    if dot_op == ".remove":
-        vars.pop( args[1] )
+    # if dot_op == ".remove":
+    #     del vars[args[0]]
 
 def header():
     return """(set-option :global-decls true)
@@ -224,6 +237,7 @@ def make_guess(arg,lessthan):
         smt_proc_write( "(pop 1)\n" ) 
         return False
     elif line == "" or line == "unsat":
+        print arg
         return True
     else:
         raise Exception("SMT lib exception: "+line)
@@ -254,4 +268,3 @@ if __name__=="__main__":
     smt_proc_write( "(exit)\n" )
     smt_proc.wait()
     log_file.close()
-
