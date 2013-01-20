ee86050c625bbc8839b79aecbf9427ad982841cc
Billy Melicher <wrm2ja@virginia.edu>
2013-01-20 17:23:30 -0500

    optimize does not default to low level compiling

--- optimize.sh
+++ optimize.sh
@@ -4,12 +4,11 @@ OUTFILE=`basename $1`.opt
 TEMPFILE=.`basename $1`.temp
 MIDFILE="$TEMPFILE".gate
 echo | cat $1 - | tac $1 | python optimize.py - | tac > $TEMPFILE
-java -cp dist/GCParser.jar:extlibs/commons-io-1.4.jar:extlibs/jargs.jar Test.TestParserFile -g $TEMPFILE
+# java -cp dist/GCParser.jar:extlibs/commons-io-1.4.jar:extlibs/jargs.jar Test.TestParserFile -g $TEMPFILE
 if [ "$2" != "--no-local" ]
 then
-    java -cp dist/GCParser.jar:extlibs/commons-io-1.4.jar:extlibs/jargs.jar Test.TestParserFile -o $MIDFILE > $OUTFILE
+    java -cp dist/GCParser.jar:extlibs/commons-io-1.4.jar:extlibs/jargs.jar Test.TestParserFile -o $TEMPFILE > $OUTFILE
 #    rm $MIDFILE
 else
-    mv $MIDFILE $OUTFILE
+    mv $TEMPFILE $OUTFILE
 fi
-#rm $TEMPFILE
